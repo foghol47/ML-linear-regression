0c1ae67826e049c4659ed12020d063382725ae1b
foghol47 <foghol47@aut.ac.ir>
2022-03-08 16:52:07 +0330

    add normalize data method

--- dataset_util.py
+++ dataset_util.py
@@ -1,8 +1,10 @@
 import random
+import tensorflow as tf
 
 class DatasetUtil():
 
-    def create_dataset(self, m: int):
+    @staticmethod
+    def create_dataset(m: int):
         'create a linear regression dataset near line y = 2x + 3'
         X = []
         Y = []
@@ -13,4 +15,8 @@ class DatasetUtil():
             X.append(x)
             Y.append(y)
         
-        return (X, Y)    
+        return (X, Y)
+
+    @staticmethod
+    def normalize_data(X):
+        return (X - tf.reduce_min(X, axis=0)) / (tf.reduce_max(X, axis=0) - tf.reduce_min(X, axis=0))
